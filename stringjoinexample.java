4133463e5a771344e6752213c018398d48cdd063
paditya2002 <p.aditya0208@gmail.com>
2023-02-12 22:29:09 +0530

    update
    intial commit

---
+++ stringjoinexample.java
@@ -0,0 +1,6 @@
+public class stringjoinexample {
+    public static void main(String args[]){
+        String joinString1=String.join("-","welcome","to","javatpoint");
+        System.out.println(joinString1);
+    }
+}
