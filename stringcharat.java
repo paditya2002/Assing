e2eca975bdb8aef86e86ea18cf41fc13f452178b
paditya2002 <p.aditya0208@gmail.com>
2023-02-12 21:56:13 +0530

    update
    intial commit

---
+++ stringcharat.java
@@ -0,0 +1,10 @@
+public class stringcharat {
+    public stringcharat() {
+    }
+
+    public static void main(String[] args) {
+        String name = "javatpoint";
+        char ch = name.charAt(4);
+        System.out.println(ch);
+    }
+}
