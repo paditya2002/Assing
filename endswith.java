b7f6831669a3a26578fb7ef3019f594ef1409149
paditya2002 <p.aditya0208@gmail.com>
2023-02-12 22:05:30 +0530

    update
    intial commit

---
+++ endswith.java
@@ -0,0 +1,8 @@
+public class endswith {
+    public static void main(String args[]) {
+        String s1 = "java by javatpoint";
+        System.out.println(s1.endsWith("t"));
+        System.out.println(s1.endsWith("point"));
+
+    }
+}
